e2cd0a0325a739169496f0af26b9682960b7089e
Akira Takagi <tkgakrwork@gmail.com>
2026-05-07 22:25:29 +0900

    step-3: 属性バインディング

--- src/tutorial/step-03/App.vue
+++ src/tutorial/step-03/App.vue
@@ -5,7 +5,7 @@ const titleClass = ref('title')
 </script>
 
 <template>
-  <h1>Make me red</h1> <!-- add dynamic class binding here -->
+  <h1 :class="titleClass">Make me red</h1> <!-- add dynamic class binding here -->
 </template>
 
 <style>
