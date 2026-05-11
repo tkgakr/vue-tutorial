1f243b3a2f887aaebe4b6e4ba9e0138abc32440c
Akira Takagi <tkgakrwork@gmail.com>
2026-05-11 21:33:49 +0900

    step-10: ウォッチャー

--- src/tutorial/step-10/App.vue
+++ src/tutorial/step-10/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const todoId = ref(1)
 const todoData = ref(null)
@@ -13,6 +13,8 @@ async function fetchData() {
 }
 
 fetchData()
+
+watch(todoId, fetchData)
 </script>
 
 <template>
